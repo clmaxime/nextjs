a96b8a09a6e504ba299051718f535812b09adb12
clmaxime <maxime.cluzel@ecole-isitech.Fr>
2025-02-03 15:28:07 +0100

    Add subsets to lusitana font

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,4 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const luistana = Lusitana({ weight: "400" });
+export const luistana = Lusitana({ weight: ["400","700"], subsets: ['latin'] });
