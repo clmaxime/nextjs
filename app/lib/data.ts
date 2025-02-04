d9e9f4acae17d26020f87b562881db3920079e14
clmaxime <maxime.cluzel@ecole-isitech.Fr>
2025-02-04 12:24:32 +0100

    add fetchCardData component- Chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,20 +8,21 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
+ 
+// ...
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
+    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
