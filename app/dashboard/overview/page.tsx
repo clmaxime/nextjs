9fac1154b75976d053375fa5d775bee2bd06b0b1
clmaxime <maxime.cluzel@ecole-isitech.Fr>
2025-02-07 12:42:21 +0100

    Chapter 15: Done

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -3,7 +3,7 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
@@ -17,7 +17,7 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h1>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
